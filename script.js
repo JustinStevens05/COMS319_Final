cb726915accb4c357dd7bd4c2a19d7e0faa0336d
JustinStevens05 <83788210+JustinStevens05@users.noreply.github.com>
2024-10-16 19:46:09 -0500

    added data.json and script.js

---
+++ script.js
@@ -0,0 +1,119 @@
+function createProductsPage(data){
+    var Card = document.getElementById("col");
+    Card.innerHTML = "";
+    products = data.products;
+    for (let i = 0; i < products.length; i++) {
+        let desc = products[i].description;
+        let image = products[i].image;
+        let url = products[i].url;
+        let imageAlt = products[1].imageAlt;
+
+        // construct the HTML element
+        let AddCard = document.createElement("div");
+        AddCard.classList.add("col"); // Add Bootstrap class to the column
+        AddCard.innerHTML = `
+            <div class="card shadow-sm">
+            <img src="${image}" alt="${imageAlt}">
+            <div class="card-body">
+              <p class="card-text">${desc}</p>
+              <a href="${url}" target="_blank">Purchase</a>
+            </div>
+          </div>
+        `;
+        Card.appendChild(AddCard);
+    }
+}
+
+function createRobotsPage(data){
+  var dCard = document.getElementById("deltaBots");
+  var oCard = document.getElementById("otherBots");
+  dCard.innerHTML = "";
+  oCard.innerHTML = "";
+  temp = data.deltasRobots;
+  for (let i = 0; i < temp.length; i++) {
+      let desc = temp[i].description;
+      let image = temp[i].image;
+      let url = temp[i].url;
+      let imageAlt = temp[1].imageAlt;
+
+      // construct the HTML element
+      let AddCard = document.createElement("div");
+      AddCard.classList.add("col"); // Add Bootstrap class to the column
+      AddCard.innerHTML = `
+          <div class="card shadow-sm">
+          <img src="${image}" alt="${imageAlt}">
+          <div class="card-body">
+            <p class="card-text">${desc}</p>
+            <a href="${url}" target="_blank">Purchase</a>
+          </div>
+        </div>
+      `;
+      dCard.appendChild(AddCard);
+  }
+
+  temp = data.otherRobots;
+  for (let i = 0; i < temp.length; i++) {
+      let desc = temp[i].description;
+      let image = temp[i].image;
+      let url = temp[i].url;
+      let imageAlt = temp[1].imageAlt;
+
+      // construct the HTML element
+      let AddCard = document.createElement("div");
+      AddCard.classList.add("col"); // Add Bootstrap class to the column
+      AddCard.innerHTML = `
+          <div class="card shadow-sm">
+          <img src="${image}" alt="${imageAlt}">
+          <div class="card-body">
+            <p class="card-text">${desc}</p>
+            <a href="${url}" target="_blank">Purchase</a>
+          </div>
+        </div>
+      `;
+      oCard.appendChild(AddCard);
+  }
+}
+
+function createIdeasPage(data){
+  var Card = document.getElementById("col");
+  Card.innerHTML = "";
+  temp = data.ideas;
+  for (let i = 0; i < temp.length; i++) {
+      let title = temp[i].title;
+      let mainT = temp[i].mainText;
+      let subT = temp[i].subText;
+
+      // construct the HTML element
+      let AddCard = document.createElement("div");
+      AddCard.classList.add("col"); // Add Bootstrap class to the column
+      AddCard.innerHTML = `
+        <div class="card shadow-sm">
+          <div class="card-body">
+            <h2>${title}</h2>
+            <p class="card-text">${mainT}</p>
+            <p class="card-text">${subT}</p>
+          </div>
+        </div>
+      `;
+      Card.appendChild(AddCard);
+  }
+}
+
+addEventListener("DOMContentLoaded",(event)=>{
+  if(document.title == "Products"){
+    fetch("./data.json")
+    .then((response) => response.json())
+    .then((data) => createProductsPage(data))
+    .catch((error) => console.log("Error: " + error))
+  } else if (document.title == "Robots") {
+    fetch("./data.json")
+    .then((response) => response.json())
+    .then((data) => createRobotsPage(data))
+    .catch((error) => console.log("Error: " + error))
+  } else if (document.title == "Ideas") {
+    fetch("./data.json")
+    .then((response) => response.json())
+    .then((data) => createIdeasPage(data))
+    .catch((error) => console.log("Error: " + error))
+  }
+});
