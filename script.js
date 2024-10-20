3188dd4f5b05d39f52bbe205e57266b85c439b87
JustinStevens05 <83788210+JustinStevens05@users.noreply.github.com>
2024-10-20 17:31:22 -0500

    added Sam's changes

--- script.js
+++ script.js
@@ -99,6 +99,34 @@ function createIdeasPage(data){
   }
 }
 
+function createPeoplePage(data){
+  var Card = document.getElementById("col");
+  Card.innerHTML = "";
+  temp = data.people;
+  for (let i = 0; i < temp.length; i++) {
+      let name = temp[i].name;
+      let email = temp[i].email;
+      let prof = temp[i].profession;
+      let git = temp[i].github;
+
+      // construct the HTML element
+      let AddCard = document.createElement("div");
+      AddCard.classList.add("col"); // Add Bootstrap class to the column
+      AddCard.innerHTML = `
+        <div class="card shadow-sm">
+          <div class="card-body">
+            <h2>${name}</h2>
+            <p class="card-text">${email}</p>
+            <p class="card-text">${prof}</p>
+            <p class="card-text"><a href= "${git}">Check out our other Git projects!</a></p>
+          </div>
+        </div>
+      `;
+      Card.appendChild(AddCard);
+  }
+}
+
+
 addEventListener("DOMContentLoaded",(event)=>{
   if(document.title == "Products"){
     fetch("./data.json")
@@ -115,5 +143,11 @@ addEventListener("DOMContentLoaded",(event)=>{
     .then((response) => response.json())
     .then((data) => createIdeasPage(data))
     .catch((error) => console.log("Error: " + error))
+  } else if (document.title == "Students") {
+    fetch("./data.json")
+    .then((response) => response.json())
+    .then((data) => createPeoplePage(data))
+    .catch((error) => console.log("Error: " + error))
   }
+  
 });
